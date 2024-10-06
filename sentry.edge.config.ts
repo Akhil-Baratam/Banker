44382d555a52cc5e057682bf05037d6ef1a6e364
Akhil Baratam <thecosmicthinkerab@gmail.com>
2024-10-06 11:56:03 +0530

    Implemented auth, appwrite and sentry

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b03d46d7a5b89790a70acdf12920c34d@o4508054589210624.ingest.de.sentry.io/4508054591111248",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
